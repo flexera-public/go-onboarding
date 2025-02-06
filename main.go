3366434ac2e5bce9f863e420bdd69d3734e5cdc4
Bryan Karaffa <bryankaraffa@gmail.com>
2025-02-06 22:09:32 +0000

    feat: add detectCloudShell()

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func detectCloudShell() string {
+	if os.Getenv("AWS_EXECUTION_ENV") == "CloudShell" {
+		return "AWS Cloud Shell"
+	}
+	if os.Getenv("CLOUD_SHELL") == "true" {
+		return "GCP Cloud Shell"
+	}
+	if os.Getenv("ACC_TERM") != "" || os.Getenv("AZUREPS_HOST_ENVIRONMENT") != "" {
+		return "Azure Cloud Shell"
+	}
+	return "local environment"
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Running in", detectCloudShell())
 }
